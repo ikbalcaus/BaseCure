c2aa99b93c08cb7f4e615be55e263adf8d85dd02
Ikbal Čaušević <ikbalcaus@gmail.com>
2023-12-03 22:45:51 +0100

    Added landing page

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
