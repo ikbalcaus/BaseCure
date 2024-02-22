93485273f7d4398378a70a856564520a28a7bbda
Ikbal Čaušević <ikbalcaus@gmail.com>
2024-02-22 15:58:23 +0100

    Added route transition animation

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -2,14 +2,29 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { trigger, query, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet, NavbarComponent]
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  animations: [
+    trigger("routerTransition", [
+      transition("* <=> *", [
+        query(":enter", [
+          style({ opacity: 0 })
+        ], { optional: true } ),
+        query(":leave", [
+          animate(50, style({ opacity: 1 }))
+        ], { optional: true } )
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-    
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet;
+  }
 }
--- AngularApp/src/app/app.config.ts
+++ AngularApp/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -7,7 +7,7 @@ import { InterceptorService } from './services/interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideClientHydration(),
     provideHttpClient(withFetch(), withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true }
