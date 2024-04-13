87d95dc55dc16e597402393bc0a9f7f91fb46cbe
Abdullah-Salcinovic <abdullah.salcinovic@gmail.com>
2024-04-13 19:01:32 +0200

    Update Lijekovi.cs

--- BaseCureAPI/BaseCureAPI/DB/Models/Lijekovi.cs
+++ BaseCureAPI/BaseCureAPI/DB/Models/Lijekovi.cs
@@ -7,7 +7,7 @@ namespace BaseCureAPI.DB.Models
     {
         public Lijekovi()
         {
-            
+
             Receptis = new HashSet<Recepti>();
         }
 
@@ -17,10 +17,10 @@ namespace BaseCureAPI.DB.Models
         public byte[]? SlikaLijeka { get; set; }
         public int? UstanovaId { get; set; }
         public string? OpisLijeka { get; set; }
-        
+
 
         public virtual UstanoveZdravstva? Ustanova { get; set; }
-        
+
         public virtual ICollection<Recepti> Receptis { get; set; }
     }
 }
