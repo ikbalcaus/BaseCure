98012e2f2ec245ddd044e255c3a23fecbfa3f96a
Ikbal Čaušević <ikbalcaus@gmail.com>
2024-05-12 16:34:29 +0200

    Added search doctors

---
+++ AngularApp/src/app/pages/user/user-about-medical-institucion/user-about-medical-institucion.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { serverSettings } from '../../../server-settings';
+
+@Component({
+  selector: 'app-user-about',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './user-about-medical-institucion.component.html',
+  styleUrl: './user-about-medical-institucion.component.css'
+})
+export class UserAboutMedicalInstitutionComponent {
+  constructor(
+    private httpClient: HttpClient,
+    private route: ActivatedRoute
+  ) {}
+
+  res: any;
+
+  ngOnInit() {
+    this.httpClient.get(serverSettings.address + "/ustanveZdravstva?id=" + this.route.snapshot.paramMap.get("id")).subscribe(
+      res => this.res = res
+    );
+  }
+}
---
+++ BaseCureAPI/BaseCureAPI/Endpoints/Ljekar/GetById/LjekarGetByIdEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using BaseCureAPI.DB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseCureAPI.Endpoints.Ljekar.GetById
+{
+    [Route("ljekari")]
+    [ApiController]
+    public class LjekariController : ControllerBase
+    {
+        private readonly BasecureContext _context;
+
+        public LjekariController(BasecureContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get([FromRoute] int id)
+        {
+            var ljekar = _context.Ljekaris.Where(x => x.LjekarId == id)
+                .Include(x => x.Korisnik)
+                .ThenInclude(x => x.Grad)
+                .Select(x => new LjekarGetByIdRes()
+                {
+                    LjekarId = x.LjekarId,
+                    Ime = x.Korisnik.Ime,
+                    Prezime = x.Korisnik.Prezime,
+                    Specijalizacija = x.Specijalizacija,
+                    MailAdresa = x.Korisnik.MailAdresa,
+                    Adresa = x.Korisnik.Adresa,
+                    Grad = x.Korisnik.Grad.Naziv
+                }).FirstOrDefault();
+
+            return Ok(ljekar);
+        }
+    }
+}
