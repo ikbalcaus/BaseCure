7456ac3a231dc2a0d938ad234600a4821f4e3353
Abdullah-Salcinovic <abdullah.salcinovic@gmail.com>
2024-07-11 16:34:35 +0200

    Added latitude and longitude to UstanoveZdravstva and created endpoints

---
+++ BaseCureAPI/BaseCureAPI/Endpoints/UstanovaZdravstva/GetLatLong/GetLatLongRes.cs
@@ -0,0 +1,9 @@
+﻿namespace BaseCureAPI.Endpoints.UstanovaZdravstva.GetLatLong
+{
+    public class GetLatLongRes
+    {
+        public int UstanovaId { get; set; }
+        public double? Lat { get; set; }
+        public double? Long { get; set; }
+    }
+}
