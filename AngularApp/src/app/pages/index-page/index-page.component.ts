1642703d18cca19171751cdde423a6825790256b
Abdullah-Salcinovic <abdullah.salcinovic@gmail.com>
2024-08-09 02:00:11 +0200

    Added logic for password modification on frontend and backend

--- AngularApp/src/app/pages/index-page/index-page.component.ts
+++ AngularApp/src/app/pages/index-page/index-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-index-page',
@@ -11,9 +12,36 @@ import { AuthService } from '../../services/auth.service';
     styleUrl: './index-page.component.css',
 })
 export class IndexPageComponent {
-    constructor(private authService: AuthService) {}
+    isForgotPasswordModalOpen = false;
+
+    constructor(private authService: AuthService, private router: Router) {}
 
     loginUser(data: any) {
         this.authService.loginUser("/auth/login", data);
+    }
+
+    openForgotPasswordModal() {
+        this.isForgotPasswordModalOpen = true;
+    }
+
+    closeForgotPasswordModal() {
+        this.isForgotPasswordModalOpen = false;
+    }
+
+    resetPassword(data: any) {
+        if (data.newPassword !== data.confirmPassword) {
+            this.authService.setAlert("danger", "Šifre se ne poklapaju!");
+            return;
+        }
+        
+        this.authService.checkEmailAndResetPassword(data.email, data.newPassword).subscribe(
+            () => {
+                this.authService.setAlert("success", "Šifra uspešno resetovana!");
+                this.closeForgotPasswordModal();
+            },
+            () => {
+                this.authService.setAlert("danger", "Došlo je do greške prilikom resetovanja šifre.");
+            }
+        );
     }
 }
